41305efd9e38dc4e7e5b7fe72747128543765e25
Eñaut Agirre Arri <enaut.irakasle@gmail.com>
2024-01-16 09:24:27 +0100

    adapatar ejemplo a los cambios de clase

--- src/main/App.java
+++ src/main/App.java
@@ -6,17 +6,17 @@ import clases.EmpleadoPorHoras;
 public class App {
 
 	public static void main(String[] args) {
-		EmpleadoAsalariado ea1 = new EmpleadoAsalariado();
+		EmpleadoAsalariado ea1 = new EmpleadoAsalariado("Koldo");
 		
-		ea1.setNombre("Koldo");
+//		ea1.setNombre("Koldo");
 		ea1.setSalario(2000);
 		System.out.println("Empleado: " + ea1.getNombre());
 		System.out.println("Salario: " + ea1.calcularSalario());
 		
-		EmpleadoPorHoras eh1 = new EmpleadoPorHoras();
+		EmpleadoPorHoras eh1 = new EmpleadoPorHoras("Iker", 8);
 		eh1.setSalario(12);
-		eh1.setNombre("Iker");
-		eh1.setHorasTrabajadas(8);
+//		eh1.setNombre("Iker");
+//		eh1.setHorasTrabajadas(8);
 		
 		System.out.println("Empleado: " + eh1.getNombre());
 		System.out.println("Salario: " + eh1.calcularSalario());
