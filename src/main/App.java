7c75da40a9b647889158e0d0a0d9972fc4f0a0b0
Eñaut Agirre Arri <enaut.irakasle@gmail.com>
2024-01-16 09:13:20 +0100

    pruebas realizadas

---
+++ src/main/App.java
@@ -0,0 +1,26 @@
+package main;
+
+import clases.EmpleadoAsalariado;
+import clases.EmpleadoPorHoras;
+
+public class App {
+
+	public static void main(String[] args) {
+		EmpleadoAsalariado ea1 = new EmpleadoAsalariado();
+		
+		ea1.setNombre("Koldo");
+		ea1.setSalario(2000);
+		System.out.println("Empleado: " + ea1.getNombre());
+		System.out.println("Salario: " + ea1.calcularSalario());
+		
+		EmpleadoPorHoras eh1 = new EmpleadoPorHoras();
+		eh1.setSalario(12);
+		eh1.setNombre("Iker");
+		eh1.setHorasTrabajadas(8);
+		
+		System.out.println("Empleado: " + eh1.getNombre());
+		System.out.println("Salario: " + eh1.calcularSalario());
+
+	}
+
+}
